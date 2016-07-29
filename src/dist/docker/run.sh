d275102cf62a98fccbabd7778790f0208894d3d7
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-07-29 18:17:07 +0200

    Remove generic DAQ packaging stuff, retrieve it from c2mon-daq-parent instead

---
+++ src/dist/docker/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run --rm --name daq-opcua -ti --net=host -e "C2MON_PORT_61616_TCP=tcp://localhost:61616" docker.cern.ch/c2mon-project/daq-opcua bin/C2MON-DAQ-STARTUP.jvm -f $@
