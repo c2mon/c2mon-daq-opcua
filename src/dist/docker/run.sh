e89ba5085f6fc614fc4cfff58d2f06b561e693c6
Justin Lewis Salmon <justin.lewis.salmon@cern.ch>
2016-09-28 15:53:44 +0200

    Update docker config

--- src/dist/docker/run.sh
+++ src/dist/docker/run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run --rm --name daq-opcua -ti --net=host -e "C2MON_PORT_61616_TCP=tcp://localhost:61616" docker.cern.ch/c2mon-project/daq-opcua bin/C2MON-DAQ-STARTUP.jvm -f $@
+docker run --rm --name daq-opcua -ti --net=host -e "C2MON_PORT_61616_TCP=tcp://localhost:61616" \
+  gitlab-registry.cern.ch/c2mon/c2mon-daq-opcua bin/C2MON-DAQ-STARTUP.jvm -f $@
