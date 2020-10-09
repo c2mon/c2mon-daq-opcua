e50f8128c48bead02c2b2e83930d0ecca239acc7
Elisabeth Stockinger <estockinger@pcbe16475.cern.ch>
2020-10-09 10:49:08 +0200

    The EquipmentScope must be registered before initializeForEquipment, otherwise Spring would inject a separate instance

--- src/main/java/cern/c2mon/daq/opcua/OPCUAMessageHandler.java
+++ src/main/java/cern/c2mon/daq/opcua/OPCUAMessageHandler.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
@@ -214,9 +214,9 @@ public class OPCUAMessageHandler extends EquipmentMessageHandler implements IEqu
     private void initializeScope(String equipmentName) {
         ApplicationContext ctx = getContext();
         scope = new EquipmentScope();
+        ((ConfigurableBeanFactory) ctx.getAutowireCapableBeanFactory()).registerScope("equipment", scope);
         scope.setExporter(ctx.getBean(AppConfig.class).mBeanExporter());
         scope.initializeForEquipment(equipmentName, ctx);
-        ((ConfigurableBeanFactory) ctx.getAutowireCapableBeanFactory()).registerScope("equipment", scope);
         controller = ctx.getBean(Controller.class);
         dataTagHandler = ctx.getBean(IDataTagHandler.class);
         commandTagHandler = ctx.getBean(CommandTagHandler.class);
